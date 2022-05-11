02357f4e9499b3c7b09758b074abfbc826f94a22
simoneluca00 <si.zaino@libero.it>
2022-05-11 11:16:51 +0200

    Correction

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// 1) creazione classe Movie
+// 1) NOTE creazione classe Movie
 class Movie {
     public $title;
     public $year;
@@ -8,15 +8,16 @@ class Movie {
     public $direction;
     public $rentalPrice;
 
-    // 2) impostazione del costruttore
+    // 2) NOTE impostazione del costruttore
     public function __construct($_title, $_year, $_originalLanguage, $_direction) {
         $this -> title = $_title;
         $this -> year = $_year;
         $this -> originalLanguage = $_originalLanguage;
         $this -> direction = $_direction;
+        $this -> setRentalPrice($_year);
     }
 
-    // 3) funzione che stabilisce il prezzo per il noleggio
+    // 3) NOTE funzione che stabilisce il prezzo per il noleggio
     public function setRentalPrice( $year ){
         if ($year < 2020) {
             $this->rentalPrice = 20;
@@ -25,30 +26,30 @@ class Movie {
         }
     }
 
-    // 4) funzione che restituisce il valore della proprietà "rentalPrice"
+    // 4) NOTE funzione che restituisce il valore della proprietà "rentalPrice"
     public function getRentalPrice(){
         return $this->rentalPrice;
     }
 }
 
-// 5) istanza per "The Wolf Of Wall Street"
+// 5) NOTE istanza per "The Wolf Of Wall Street"
 $TheWolfOfWallStreet = new Movie("The Wolf Of Wall Street", 2013, "English", "Martin Scorsese");
 
-// 5) istanza per "È stata la mano di Dio"
+// 5) NOTE istanza per "È stata la mano di Dio"
 $eStataLaManoDiDio = new Movie("È stata la mano di Dio", 2021, "Italian", "Paolo Sorrentino");
 
 
-// 6) utilizzare metodo della classe per stabilire il prezzo
-$TheWolfOfWallStreet -> setRentalPrice( $TheWolfOfWallStreet -> year );
-$eStataLaManoDiDio -> setRentalPrice( $eStataLaManoDiDio -> year );
+// 6) NOTE utilizzare metodo della classe per stabilire il prezzo --> invece di impostarlo in questo modo è possibile attivare la funzione direttamente nel construct
+// $TheWolfOfWallStreet -> setRentalPrice( $TheWolfOfWallStreet -> year );
+// $eStataLaManoDiDio -> setRentalPrice( $eStataLaManoDiDio -> year );
 
-// 7) stampa dei valori per "The Wolf Of Wall Street"
+// 7) NOTE stampa dei valori per "The Wolf Of Wall Street"
 
 echo "<p>" . 
         "1) Il film " . $TheWolfOfWallStreet -> title . ", uscito nel " . $TheWolfOfWallStreet -> year . " e diretto da " . $TheWolfOfWallStreet -> direction . ", ha un prezzo di " . $TheWolfOfWallStreet -> getRentalPrice() . "€;" .
     "</p>";
 
-// 7) stampa dei valori per "È stata la mano di Dio"
+// 7) NOTE stampa dei valori per "È stata la mano di Dio"
 echo "<p>" . 
         "2) Il film " . $eStataLaManoDiDio -> title . ", uscito nel " . $eStataLaManoDiDio -> year . " e diretto da " . $eStataLaManoDiDio -> direction . ", ha un prezzo di " . $eStataLaManoDiDio -> getRentalPrice() . "€;" .
     "</p>";
